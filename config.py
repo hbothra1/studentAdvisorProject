2b24185770090d4ccfe726657e51c0f69e1c4279
Hemant Bothra <hemantbothra@Hemants-MacBook-Air.local>
2025-05-07 13:15:31 -0700

    added config.py, changed location of course_database.db to /data

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Get the project root directory
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Database path
+DB_PATH = os.path.join(PROJECT_ROOT, 'data', 'course_database.db')
